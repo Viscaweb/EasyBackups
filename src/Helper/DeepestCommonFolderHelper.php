6c022bacf76e25bba04d0b3bc4e80ce0118306a6
Jonas HAOUZI <jonas@viscaweb.com>
2016-06-25 01:32:57 +0200

    Service calculating the deepest common folders for the given files

---
+++ src/Helper/DeepestCommonFolderHelper.php
@@ -0,0 +1,76 @@
+<?php
+namespace Helper;
+
+use Models\File;
+
+class DeepestCommonFolderHelper
+{
+    /**
+     * @param File[] $files
+     *
+     * @return null|string
+     */
+    public function findDeepest($files)
+    {
+        $pathsPerDepth = [];
+
+        foreach ($files as $file) {
+            $folder = $this->findPath($file->getPath());
+            foreach($this->explodePath($folder) as $depth => $path){
+                if (!isset($pathsPerDepth[$depth][$path])){
+                    $pathsPerDepth[$depth][$path] = 0;
+                }
+                $pathsPerDepth[$depth][$path]++;
+            }
+        }
+
+        krsort($pathsPerDepth);
+
+        $totalFiles = count($files);
+        foreach($pathsPerDepth as $depth => $paths){
+            foreach($paths as $path => $numberOfFilesWithThisPath){
+                if ($numberOfFilesWithThisPath === $totalFiles){
+                    return $path;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string[]
+     */
+    private function explodePath($path)
+    {
+        $allPaths = [];
+
+        $pathStructure = '';
+        $depth = 0;
+        foreach (explode('/', $path) as $part) {
+            if (empty($part)){
+                continue;
+            }
+            $depth++;
+            $pathStructure .= '/'.$part;
+            $allPaths[$depth] = $pathStructure;
+        }
+
+        krsort($allPaths);
+
+        return $allPaths;
+    }
+
+    /**
+     * @param $file
+     *
+     * @return string
+     */
+    private function findPath($file)
+    {
+        return preg_replace('#^(.*)\/?[^\/]+$#U', '$1', $file);
+    }
+
+}
---
+++ tests/unit/DeepestCommonFilesFolderHelperTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Helper\DeepestCommonFolderHelper;
+use Models\File;
+
+class DeepestCommonFolderHelperTest extends PHPUnit_Framework_TestCase
+{
+    /** @var DeepestCommonFolderHelper */
+    private $helper;
+    
+    public function setUp()
+    {
+        $this->helper = new DeepestCommonFolderHelper();
+    }
+
+    /** @test */
+    public function testRegularStructureFindsDeepestFolder()
+    {
+        $deepestFolder = $this->helper->findDeepest([
+            new File('/folder-1/folder-2/folder-3/file1.txt'),
+            new File('/folder-1/folder-2/folder-3/file2.txt'),
+            new File('/folder-1/folder-2/folder-3/file3.txt'),
+        ]);
+
+        $this->assertEquals('/folder-1/folder-2/folder-3', $deepestFolder);
+    }
+
+    /** @test */
+    public function testWeirdStructureFindsDeepestFolder()
+    {
+        $deepestFolder = $this->helper->findDeepest([
+            new File('/folder-1/folder-2/folder-3/file1.txt'),
+            new File('/folder-1/folder-2/folder-4/file2.txt'),
+            new File('/folder-1/folder-2/file3.txt'),
+        ]);
+
+        $this->assertEquals('/folder-1/folder-2', $deepestFolder);
+    }
+}
